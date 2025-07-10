9b10379cb48e19bd39cb65b2300db1fb2f8e35d1
Morten Holt <mho@delegate.dk>
2025-07-10 09:32:58 +0200

    TEST: Add Plugin and CustomAPI sample to test sync

---
+++ Samples/SamplePlugins/AccountPlugin.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using BusinessDomain.Context;
+using SamplePlugins;
+
+namespace TestPluginAssembly365.Plugins.LegacyDaxif {
+    public class AccountPlugin : Plugin {
+
+        public AccountPlugin() : base(typeof(AccountPlugin)) {
+            RegisterPluginStep<Account>(
+                EventOperation.Update,
+                ExecutionStage.PostOperation,
+                Execute)
+                .AddFilteredAttributes(x => x.Name)
+                .AddImage(ImageType.PreImage, x => x.Name);
+
+            RegisterPluginStep<Account>(
+                EventOperation.Create,
+                ExecutionStage.PostOperation,
+                Execute);
+        }
+
+        protected void Execute(LocalPluginContext localContext) {
+            if (localContext == null) {
+                throw new ArgumentNullException(nameof(localContext));
+            }
+
+            if (localContext.PluginExecutionContext == null)
+            {
+                throw new ArgumentNullException(nameof(localContext.PluginExecutionContext));
+            }
+
+            if (localContext.OrganizationService == null)
+            {
+                throw new ArgumentNullException(nameof(localContext.OrganizationService));
+            }
+
+            if (localContext.TracingService == null)
+            {
+                throw new ArgumentNullException(nameof(localContext.TracingService));
+            }
+
+            if (localContext.OrganizationAdminService == null)
+            {
+                throw new ArgumentNullException(nameof(localContext.OrganizationAdminService));
+            }
+
+            var service = localContext.OrganizationService;
+
+            var account = GetEntity<Account>(localContext);
+            if (account == null)
+            {
+                throw new ArgumentNullException(nameof(account));
+            }
+
+            var rand = new Random();
+            service.Create(new Lead() {
+                Subject = nameof(AccountPlugin) + " " + localContext.PluginExecutionContext.MessageName + ": Some new lead " + rand.Next(0, 1000),
+                ParentAccountId = new Account(localContext.PluginExecutionContext.PrimaryEntityId).ToEntityReference()
+            });
+        }
+    }
+}
